1437e3044be621dee642295345157f76e7916dda
coder71 <kamrunnaher877@gmail.com>
2021-10-06 22:05:26 +0600

    services and moreservices component added

--- src/components/MoreServices/MoreServices.js
+++ src/components/MoreServices/MoreServices.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Container, Row } from 'react-bootstrap';
 import Service from '../Service/Service';
-
-const Services = () => {
+const MoreServices = () => {
   const Services = [
     {
-      key: 1,
+      key: '4853545077',
       img: 'https://images.unsplash.com/photo-1580894894513-541e068a3e2b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80',
       name: 'BSc Computer Science',
       price: 1200,
@@ -14,7 +13,7 @@ const Services = () => {
       isAvailable: true,
     },
     {
-      key: 2,
+      key: '3807296670',
       img: 'https://images.unsplash.com/photo-1526374965328-7f61d4dc18c5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80',
       name: 'Analysis of Algorithms',
       price: 1000,
@@ -23,17 +22,16 @@ const Services = () => {
       isAvailable: false,
     },
     {
-      key: 3,
+      key: '1290847444',
       img: 'https://images.unsplash.com/photo-1620712943543-bcc4688e7485?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8YWl8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
       name: 'AI For Everyone',
       price: 2400,
       year: 4,
       desc: 'AI is not only for engineers. If you want your organization to become better at using AI, this is the course to tell everyone--especially your non-technical colleagues--to take. ',
-
       isAvailable: true,
     },
     {
-      key: 4,
+      key: '9445869389',
       img: 'https://images.unsplash.com/photo-1533227268428-f9ed0900fb3b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8aGFwcGluZXNzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
       name: 'The science of well being',
       price: 240,
@@ -42,7 +40,7 @@ const Services = () => {
       isAvailable: true,
     },
     {
-      key: 5,
+      key: '5834188974',
       img: 'https://images.unsplash.com/photo-1612232134966-a9b076b9fbe7?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cHN5Y2hvbG9naXN0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
       name: 'Introduction to Psychology',
       price: 500,
@@ -51,7 +49,7 @@ const Services = () => {
       isAvailable: false,
     },
     {
-      key: 6,
+      key: '7180900435',
       img: 'https://images.unsplash.com/photo-1551288049-bebda4e38f71?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8ZXhjZWx8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
       name: 'Excel Skills for Business',
       price: 1200,
@@ -60,7 +58,7 @@ const Services = () => {
       isAvailable: true,
     },
     {
-      key: 7,
+      key: '0938980793',
       img: 'https://images.unsplash.com/photo-1531403009284-440f080d1e12?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cHJvamVjdCUyMG1hbmFnZW1lbnR8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
       name: 'Foundations of Project Management',
       price: 800,
@@ -69,7 +67,7 @@ const Services = () => {
       isAvailable: false,
     },
     {
-      key: 8,
+      key: '9367514638',
       img: 'https://media.istockphoto.com/photos/closeup-of-unrecognizable-designer-sitting-at-desk-with-papers-and-picture-id1192947841?b=1&k=20&m=1192947841&s=170667a&w=0&h=0ZGbZQRv4xmh5goNIyzWtIoeQGxOHAxLc-8r76WRPFE=',
       name: 'UX Design Professional Certificate',
       price: 240,
@@ -80,11 +78,8 @@ const Services = () => {
   ];
   return (
     <Container>
-      <h2 className="fs-2 text-center fw-bold mt-3">Our Services</h2>
-
-      {/* show four services */}
       <Row className="justify-content-center">
-        {Services.slice(0, 4).map((service) => (
+        {Services.map((service) => (
           <Service key={service.key} service={service} />
         ))}
       </Row>
@@ -92,4 +87,4 @@ const Services = () => {
   );
 };
 
-export default Services;
+export default MoreServices;
